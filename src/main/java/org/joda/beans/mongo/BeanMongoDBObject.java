d5a1c6dc8924f0eb979c6e1e403e9816bb9292f3
Stephen Colebourne <scolebourne@joda.org>
2010-07-01 13:43:44 +0000

    Add basic MongoDB support and tests
    git-svn-id: http://joda-beans.svn.sourceforge.net/svnroot/joda-beans/JodaBeans/trunk@39 4d2865e9-9026-0410-ac60-f820a1091da6

---
+++ src/main/java/org/joda/beans/mongo/BeanMongoDBObject.java
@@ -0,0 +1,118 @@
+/*
+ *  Copyright 2001-2010 Stephen Colebourne
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.joda.beans.mongo;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.joda.beans.Bean;
+
+import com.mongodb.DBObject;
+
+/**
+ * Allows a Joda-Bean to be passed directly to MongoDB.
+ *
+ * @author Stephen Colebourne
+ */
+public class BeanMongoDBObject implements DBObject {
+
+    /**
+     * The underlying bean.
+     */
+    private final Bean<?> bean;
+    /**
+     * The Mongo partial flag.
+     */
+    private boolean partial;
+
+    /**
+     * Creates an instance wrapping a bean.
+     * @param bean  the bean to wrap, not null
+     */
+    public BeanMongoDBObject(Bean<?> bean) {
+        this.bean = bean;
+    }
+
+    //-----------------------------------------------------------------------
+    @Override
+    public boolean containsField(String name) {
+        return bean.propertyExists(name);
+    }
+
+    /** 
+     * @deprecated Use containsField()
+     */
+    @Override
+    @Deprecated
+    public boolean containsKey(String name) {
+        return containsField(name);
+    }
+
+    @Override
+    public Object get(String name) {
+        return bean.property(name).get();
+    }
+
+    @Override
+    public Object put(String name, Object value) {
+        return bean.property(name).put(value);
+    }
+
+    @Override
+    public void putAll(DBObject object) {
+        for (String name : object.keySet()){
+            put(name, object.get(name));
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void putAll(Map map) {
+        for (Iterator it = map.keySet().iterator(); it.hasNext(); ) {
+            Object key = (Object) it.next();
+            put(key.toString(), map.get(key));
+        }
+    }
+
+    @Override
+    public Object removeField(String name) {
+        throw new UnsupportedOperationException("Remove unsupported");
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return bean.propertyMap().keySet();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map toMap() {
+        return bean.propertyMap().flatten();
+    }
+
+    //-----------------------------------------------------------------------
+    @Override
+    public boolean isPartialObject() {
+        return partial;
+    }
+
+    @Override
+    public void markAsPartialObject() {
+        partial = true;
+    }
+
+}
---
+++ src/test/java/org/joda/beans/TestPerson.java
@@ -0,0 +1,140 @@
+/*
+ *  Copyright 2001-2010 Stephen Colebourne
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.joda.beans;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test property using Person.
+ */
+@Test
+public class TestPerson {
+
+    private static final String FORENAME = "forename";
+    private static final String SURNAME = "surname";
+    private static final String NUMBER_OF_CARS = "numberOfCars";
+
+    public void test_bean() {
+        Bean<Person> test = Person.META.createBean();
+        
+        assertEquals(test.bean() instanceof Person, true);
+        
+        assertEquals(test.metaBean(), Person.META);
+        
+        assertEquals(test.propertyExists(FORENAME), true);
+        assertEquals(test.propertyExists(SURNAME), true);
+        assertEquals(test.propertyExists(NUMBER_OF_CARS), true);
+        assertEquals(test.propertyExists("Rubbish"), false);
+        
+        assertEquals(test.property(FORENAME).name(), FORENAME);
+        assertEquals(test.property(SURNAME).name(), SURNAME);
+        assertEquals(test.property(NUMBER_OF_CARS).name(), NUMBER_OF_CARS);
+    }
+
+    @Test(expectedExceptions=NoSuchElementException.class)
+    public void test_bean_invalidPropertyName() {
+        Bean<Person> test = Person.META.createBean();
+        try {
+            test.property("Rubbish");
+        } catch (NoSuchElementException ex) {
+            System.out.println(ex.getMessage());
+            throw ex;
+        }
+    }
+
+    //-----------------------------------------------------------------------
+    public void test_metaBean() {
+        MetaBean<Person> test = Person.META;
+        
+        assertEquals(test.beanClass(), Person.class);
+        
+        assertEquals(test.name(), Person.class.getName());
+        
+        assertEquals(test.metaPropertyExists(FORENAME), true);
+        assertEquals(test.metaPropertyExists(SURNAME), true);
+        assertEquals(test.metaPropertyExists(NUMBER_OF_CARS), true);
+        assertEquals(test.metaPropertyExists("Rubbish"), false);
+        
+        assertEquals(test.metaProperty(FORENAME).name(), FORENAME);
+        assertEquals(test.metaProperty(SURNAME).name(), SURNAME);
+        assertEquals(test.metaProperty(NUMBER_OF_CARS).name(), NUMBER_OF_CARS);
+        
+        Map<String, MetaProperty<Person, Object>> map = test.metaPropertyMap();
+        assertEquals(map.size(), 3);
+        assertEquals(map.containsKey(FORENAME), true);
+        assertEquals(map.containsKey(SURNAME), true);
+        assertEquals(map.containsKey(NUMBER_OF_CARS), true);
+    }
+
+    @Test(expectedExceptions=NoSuchElementException.class)
+    public void test_metaBean_invalidPropertyName() {
+        MetaBean<Person> test = Person.META;
+        try {
+            test.metaProperty("Rubbish");
+        } catch (NoSuchElementException ex) {
+            System.out.println(ex.getMessage());
+            throw ex;
+        }
+    }
+
+    //-----------------------------------------------------------------------
+    public void test_property() {
+        Person person = new Person();
+        Property<Person, String> test = person.forename();
+        
+        assertSame(test.bean(), person);
+        assertSame(test.metaProperty(), Person.FORENAME);
+        
+        assertEquals(test.get(), null);
+        person.setForename("A");
+        assertEquals(test.get(), "A");
+        test.set("B");
+        assertEquals(test.get(), "B");
+        assertEquals(test.put("C"), "B");
+        assertEquals(test.get(), "C");
+    }
+
+    //-----------------------------------------------------------------------
+    public void test_propertyMap() {
+        Person person = new Person();
+        PropertyMap<Person> test = person.propertyMap();
+        
+        assertSame(test.size(), 3);
+        assertEquals(test.containsKey(FORENAME), true);
+        assertEquals(test.containsKey(SURNAME), true);
+        assertEquals(test.containsKey(NUMBER_OF_CARS), true);
+    }
+
+    public void test_propertyMap_flatten() {
+        Person person = new Person();
+        person.setForename("A");
+        person.setSurname("B");
+        person.setNumberOfCars(3);
+        Map<String, Object> test = person.propertyMap().flatten();
+        
+        assertSame(test.size(), 3);
+        assertEquals(test.get(FORENAME), "A");
+        assertEquals(test.get(SURNAME), "B");
+        assertEquals(test.get(NUMBER_OF_CARS), 3);
+    }
+
+}
