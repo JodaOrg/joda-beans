f4a90cbaf475800c2e58d335999aea3187c8d2de
Stephen Colebourne <scolebourne@joda.org>
2010-07-15 11:50:58 +0000

    Ensure meta-property map is unmodifiable
    git-svn-id: http://joda-beans.svn.sourceforge.net/svnroot/joda-beans/JodaBeans/trunk@80 4d2865e9-9026-0410-ac60-f820a1091da6

--- src/main/java/org/joda/beans/impl/reflection/ReflectiveMetaBean.java
+++ src/main/java/org/joda/beans/impl/reflection/ReflectiveMetaBean.java
@@ -17,6 +17,7 @@ package org.joda.beans.impl.reflection;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -83,7 +84,7 @@ public final class ReflectiveMetaBean implements MetaBean {
             }
         }
         
-        this.metaPropertyMap = map;
+        this.metaPropertyMap = Collections.unmodifiableMap(map);
     }
 
     //-----------------------------------------------------------------------
