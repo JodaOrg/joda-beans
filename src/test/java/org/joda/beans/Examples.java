0e4e7ecfb074e40784ae4875aefe7aae20598dd5
Stephen Colebourne <scolebourne@joda.org>
2012-03-12 11:04:20 +0000

    Move example code to correct location

--- src/test/java/org/joda/beans/Examples.java
+++ src/test/java/org/joda/beans/Examples.java
@@ -13,9 +13,12 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.joda.beans.gen;
+package org.joda.beans;
 
 import org.joda.beans.Property;
+import org.joda.beans.gen.Address;
+import org.joda.beans.gen.Documentation;
+import org.joda.beans.gen.Person;
 
 /**
  * Examples using Person.
