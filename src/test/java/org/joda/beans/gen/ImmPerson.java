7fc5b39ba2dc8eaa2c95701f72d1220a4b767ccd
Stephen Colebourne <scolebourne@joda.org>
2013-10-14 12:10:29 +0100

    Refactor code generation avoiding two lines next to one another
    Better approach to if statements

--- src/test/java/org/joda/beans/gen/ImmPerson.java
+++ src/test/java/org/joda/beans/gen/ImmPerson.java
@@ -233,7 +233,6 @@ public final class ImmPerson implements ImmutableBean {
     }
 
     //-----------------------------------------------------------------------
-    //-----------------------------------------------------------------------
     /**
      * Returns a builder that allows this bean to be mutated.
      * @return the mutable builder, not null
