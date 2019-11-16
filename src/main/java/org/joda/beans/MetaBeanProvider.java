21cb2bb28cea4aade2b4fdf1659bb4eb3c1a4529
C.J.Kent <cjkent@hotmail.com>
2019-11-16 11:04:21 +0000

    Javadoc

--- src/main/java/org/joda/beans/MetaBeanProvider.java
+++ src/main/java/org/joda/beans/MetaBeanProvider.java
@@ -6,9 +6,15 @@
 package org.joda.beans;
 
 /**
- *
+ * A provider of {@link MetaBean} instances for bean classes.
  */
 public interface MetaBeanProvider {
 
+    /**
+     * Returns the meta bean for the class or null if no meta bean can be found.
+     *
+     * @param cls the class for which the meta bean is required
+     * @return the meta bean for the class or null if no meta bean can be found
+     */
     MetaBean findMetaBean(Class<?> cls);
 }
